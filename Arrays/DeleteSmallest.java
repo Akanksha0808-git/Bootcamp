d6a73cca8924bcba5e014dfaf0441bfe6f89fa01
Akanksha Sharma <akanksharma0829@gmail.com>
2026-06-17 00:17:40 +0530

    new commit

--- Arrays/DeleteSmallest.java
+++ Arrays/DeleteSmallest.java
@@ -1,25 +1,24 @@
 import java.util.*;
 public class DeleteSmallest {
-    public static void mai(String args[]){
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n= sc.nextInt();
         int[] arr= new int[n];
-        for(int i =0;i>n;i++){
-          arr[n]=sc.nextInt();
+        for(int i =0;i<n;i++){
+          arr[i]=sc.nextInt();
         }
-        double small;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0){
-                continue;
+        int small=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]< arr[small])
+            {
+                small =i;
             }
-            else{
-                if(small<arr[i]){
-                    small=arr[i];
-                    arr.remove(small);
-                }
+         }
+        for(int i =0;i<n;i++){
+            if(i!= small){
+                System.out.print(arr[i]+" ");
             }
         }
-        
-return small;
     }
 }
