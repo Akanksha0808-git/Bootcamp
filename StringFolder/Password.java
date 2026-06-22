583c674350a8ea44478c1722dc38b58ace0bb1f9
Akanksha Sharma <akanksharma0829@gmail.com>
2026-06-23 01:06:12 +0530

    new commit

---
+++ StringFolder/Password.java
@@ -0,0 +1,12 @@
+package StringFolder;
+
+import java.util.Scanner;
+
+public class Password {
+     public static void main(String agrs[]){
+    Scanner sc = new Scanner(System.in);
+    String str=sc.nextLine();
+    str= str.toLowerCase();
+sc.close();
+}
+}
