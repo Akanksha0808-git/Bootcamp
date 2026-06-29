4488029f1ec7347448c7329f04f3aea2c277a9ab
Akanksha Sharma <akanksharma0829@gmail.com>
2026-06-29 23:55:39 +0530

    commit

---
+++ StringFolder/FirstNonRepeatingChar.java
@@ -0,0 +1,23 @@
+package StringFolder;
+import java.util.*;
+public class FirstNonRepeatingChar {
+  Scanner sc =new Scanner(System.in);
+  String str= sc.nextLine();
+  for(int i =0;i<str.length();i++){
+     int count=0;
+     for(int j=0;j<str.length();j++){
+        if(str.charAt(i)==str.charAt(j)){
+            count++;
+        }
+     }
+    
+     if(count==1){
+        System.out.print(str.charAt(i));
+        break;
+     }
+    }
+     sc.close();
+
+  }
+}
+
