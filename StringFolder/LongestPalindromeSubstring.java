c5aea386c3b3033c380e38ee1c716e0047193f63
Akanksha Sharma <akanksharma0829@gmail.com>
2026-06-30 23:54:57 +0530

    new prgm commit

--- StringFolder/LongestPalindromeSubstring.java
+++ StringFolder/LongestPalindromeSubstring.java
@@ -1,36 +1,35 @@
+package StringFolder;
 class Solution {
-    public String longestPalindrome(String s) {
+    public static void main(String[] args) {
 
-        if (s == null || s.length() < 2)
-            return s;
-
-        int start = 0;
-        int end = 0;
+        String s = "babad";
+        String ans = "";
 
         for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
 
-            int len1 = expand(s, i, i);       // Odd length
-            int len2 = expand(s, i, i + 1);   // Even length
+                String sub = s.substring(i, j + 1);
 
-            int len = Math.max(len1, len2);
+                // Check if substring is palindrome
+                int left = 0;
+                int right = sub.length() - 1;
+                boolean isPalindrome = true;
 
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+                while (left < right) {
+                    if (sub.charAt(left) != sub.charAt(right)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    left++;
+                    right--;
+                }
+
+                if (isPalindrome && sub.length() > ans.length()) {
+                    ans = sub;
+                }
             }
         }
 
-        return s.substring(start, end + 1);
-    }
-
-    private int expand(String s, int left, int right) {
-
-        while (left >= 0 && right < s.length()
-                && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-
-        return right - left - 1;
+        System.out.println("Longest Palindrome: " + ans);
     }
 }
